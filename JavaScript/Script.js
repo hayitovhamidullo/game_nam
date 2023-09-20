276101987c1fe6e0d9c069b409df606a49fb3934
Hamidullo <hayitovhamidullo641@gmail.com>
2023-09-20 11:16:20 +0500

    salom

--- JavaScript/Script.js
+++ JavaScript/Script.js
@@ -1,5 +1,7 @@
 'use strict';
-let x;
+let ScoreNam = 20;
+let HighscoreNmaNum = 20;
+let x = Math.floor(Math.random() * 20 ) + 1;
 document.querySelector(`.uzbek`).addEventListener(`click`, function(){
     document.querySelector(`.between`).textContent = `Kiritiladigan solar oralig'i 1 dan 20 gacha`;
     document.querySelector(`.start`).textContent = `🔮 Taxmin qilishni boshlang`;
@@ -18,7 +20,48 @@ document.querySelector(`.english`).addEventListener(`click`,function(){
     document.querySelector(`.between`).textContent = `(Between 1 and 20)`;
     document.querySelector(`h1`).textContent = `Guess My Number!`;
 });
-document.getElementById(`.guess`).onclick = function(){
-    x = match.floor(Math.random() * 20 ) + 1;
-    document.getElementById(`salom`).innerHTML = x;
-};
+document.querySelector(`.btm`).addEventListener(`click`, function(){
+    let namberNam = Number(document.querySelector(`.guess`).value);
+    if (ScoreNam>0) {
+        if (!namberNam) {
+            document.querySelector(`.start`).textContent = "🤷‍♀️ No number!";
+        }
+        else if(namberNam > x){
+            document.querySelector(`.start`).textContent = "📈 Too High!"
+            ScoreNam--;
+            document.querySelector(`.yigirma`).textContent = ScoreNam; 
+            document.querySelector(`.HighscoreNma`).textContent = HighscoreNmaNum; 
+        }
+        else if(namberNam<x){
+            document.querySelector(`.start`).textContent = "📉 Too Low!"
+            ScoreNam--;
+            document.querySelector(`.yigirma`).textContent = ScoreNam; 
+            document.querySelector(`.HighscoreNma`).textContent = HighscoreNmaNum; 
+        }
+        else if(namberNam == x){
+            document.querySelector(`.start`).textContent = "🎯Correct Number"
+            HighscoreNmaNum++;
+            document.querySelector(`.HighscoreNma`).textContent = HighscoreNmaNum; 
+            document.querySelector(`main`).style.backgroundColor = `#60b347`;
+            document.querySelector(`header`).style.backgroundColor = `#60b347`;
+            document.querySelector(`.guess`).style.backgroundColor = `#60b347`;
+            document.querySelector(".nam").textContent= x;
+            document.querySelector(`.nam`).style.width = `110px`;
+            document.querySelector(".nam").style.padding = `10px`;
+        }
+    }
+    else{
+        document.querySelector(`.start`).textContent = "😒 You lost the game";
+        document.querySelector(`.ScoreNam`).textContent = 0;
+    }
+});
+
+document.querySelector(`.Again`).addEventListener(`click`, function(){
+    x = Math.floor(Math.random() * 20) + 1;
+    document.querySelector("main").style.backgroundColor="#222";
+    document.querySelector(`.guess`).style.backgroundColor = `#222`;
+    document.querySelector("header").style.backgroundColor="#222";
+    document.querySelector(".nam").textContent= "?";
+    document.querySelector(".nam").style.padding = `10px`;
+
+});
